bd163acc5d087a6b4caa9b883c600ef3d1909960
ecgouvea <ecgouvea@hotmail.com>
2021-01-01 12:49:17 -0300

    Initial version

---
+++ src/App.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        JFrame frame = new JFrame();
+        frame.setSize(420, 420);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
